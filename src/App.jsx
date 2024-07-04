a49bb2df3e3359080ce8444826a8ec26c0d0b6c7
Vedant <vedantnagar21@gmail.com>
2024-07-04 14:56:24 +0530

    feat: Add About component to display information about the user

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -5,12 +6,13 @@ function App() {
   return (
     <>
       <div className="selection:bg-cyan-100 selection:text-cyan-200">
-        <div className="-z-10  h-full w-full">
+        <div className="-z-10 fixed h-full w-full">
           <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] "></div>
         </div>
         <div className="mx-auto px-8 container">
           <Navbar />
           <Hero />
+          <About />
         </div>
       </div>
     </>
---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="border-neutral-900">
+        <h2 className="my-auto text-center text-4xl">About Me</h2>
+      </div>
+    </>
+  );
+};
+
+export default About;
