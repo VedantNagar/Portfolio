ed28100f9775a79b67a01d5a159afb34895f87c8
Vedant <vedantnagar21@gmail.com>
2024-07-04 23:31:17 +0530

    feat: Refactor Project component to use external data source

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,31 +1,6 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa6";
-const projects = [
-  {
-    title: "Medware - Healthcare Companion",
-    description:
-      "A Healthcare Website consisting of a Disease Predictor made in React, Django, Tailwind and Machine Learning.",
-    image: "path-to-your-image-1",
-    link: "#",
-    git: "#",
-  },
-  {
-    title: "Swift Chat App",
-    description:
-      "A chat app built with WebSocket API and ws lib. MERN Stack with TailwindCSS. Feat includes realtime msg exchange and user authentication with email verification.",
-    image: "path-to-your-image-2",
-    link: "#",
-    git: "#",
-  },
-  {
-    title: "Vetmedman",
-    description:
-      "A shopping store I created for a freelance client. GithubRepo not available for this private project.",
-    image: "path-to-your-image-3",
-    link: "#",
-    git: "#",
-  },
-];
+import projects from "../content/projects.js";
 
 const Project = () => {
   return (
