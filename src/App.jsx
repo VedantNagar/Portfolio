5f47bd74b3363e7a90b354da3ef964d9a6149fd6
Vedant <vedantnagar21@gmail.com>
2024-07-04 14:52:56 +0530

    refactor: Update background styling in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] selection:bg-cyan-100 selection:text-cyan-200">
+      <div className="selection:bg-cyan-100 selection:text-cyan-200">
+        <div className="-z-10  h-full w-full">
+          <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] "></div>
+        </div>
         <div className="mx-auto px-8 container">
           <Navbar />
           <Hero />
