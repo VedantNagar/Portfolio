0058b973538f5156917fd475687d1b3c9fefec9b
Vedant <vedantnagar21@gmail.com>
2024-07-04 19:26:40 +0530

    chore: Add TechStack component to display the technology stack

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <Navbar />
           <Hero />
           <About />
+          <TechStack />
         </div>
       </div>
     </>
---
+++ src/components/TechStack.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TechStack = () => {
+  return (
+    <>
+      <div>
+        <h1 className="my-20 text-center text-4xl text-white">Tech Stack</h1>
+      </div>
+    </>
+  );
+};
+
+export default TechStack;
