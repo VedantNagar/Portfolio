62e65102a9d34d11a8d54e8de104997ce4c4bc2a
Vedant <vedantnagar21@gmail.com>
2024-07-04 23:24:06 +0530

    feat: Add GitHub links to project cards

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { FaGithub } from "react-icons/fa6";
 const projects = [
   {
     title: "Medware - Healthcare Companion",
@@ -7,6 +7,7 @@ const projects = [
       "A Healthcare Website consisting of a Disease Predictor made in React, Django, Tailwind and Machine Learning.",
     image: "path-to-your-image-1",
     link: "#",
+    git: "#",
   },
   {
     title: "Swift Chat App",
@@ -14,6 +15,7 @@ const projects = [
       "A chat app built with WebSocket API and ws lib. MERN Stack with TailwindCSS. Feat includes realtime msg exchange and user authentication with email verification.",
     image: "path-to-your-image-2",
     link: "#",
+    git: "#",
   },
   {
     title: "Vetmedman",
@@ -21,18 +23,19 @@ const projects = [
       "A shopping store I created for a freelance client. GithubRepo not available for this private project.",
     image: "path-to-your-image-3",
     link: "#",
+    git: "#",
   },
 ];
 
 const Project = () => {
   return (
-    <div className="min-h-screen  p-6 flex flex-col items-center">
+    <div className=" p-5 flex flex-col items-center">
       <h1 className="text-4xl font-bold text-white mb-8">My Projects</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="flex flex-wrap justify-center">
         {projects.map((project, index) => (
           <div
             key={index}
-            className=" bg-slate-900 text-white p-6 rounded-lg shadow-lg transform hover:scale-105 transition-transform duration-300"
+            className="bg-slate-900 text-white m-3 p-6 rounded-lg shadow-lg transform hover:scale-105 transition-transform duration-300 w-full md:w-5/12"
           >
             <img
               src={project.image}
@@ -41,9 +44,21 @@ const Project = () => {
             />
             <h3 className="text-2xl font-semibold mb-2">{project.title}</h3>
             <p className="mb-4">{project.description}</p>
-            <a href={project.link} className="text-teal-300 hover:underline">
-              View Project
-            </a>
+            <div className="flex flex-col flex-wrap">
+              <div className="flex justify-between">
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-teal-300 hover:underline"
+                >
+                  View Project
+                </a>
+                <a href={project.git} target="_blank" rel="noopener noreferrer">
+                  <FaGithub className="text-white text-3xl p-1 hover:bg-purple-500 hover:shadow-lg hover:rounded-full hover:scale-110 transition-transform duration-300 cursor-pointer" />
+                </a>
+              </div>
+            </div>
           </div>
         ))}
       </div>
