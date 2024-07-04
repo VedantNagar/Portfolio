5ab5ab93a2838daeb360c54505eec940904a7ca6
Vedant <vedantnagar21@gmail.com>
2024-07-04 16:46:22 +0530

    chore: Update Navbar component to include social icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
+import { CgMail } from "react-icons/cg";
 
 const Navbar = () => {
   return (
     <>
       <nav className="mb-20 flex items-center justify-between py-6 backdrop-blur">
         <div></div>
-        <div className="flex items-center gap-4 text-3xl m-6 pr-2">
-          <FaLinkedin className="text-[#0A66C2]" />
-          <FaGithub className="text-white" />
+        <div className="flex space-x-4">
+          <FaLinkedin className="text-[#0A66C2] text-5xl p-1 hover:bg-white hover:shadow-lg hover:rounded-full hover:scale-110 transition-transform duration-300" />
+          <FaGithub className="text-white text-5xl p-1 hover:bg-purple-500 hover:shadow-lg hover:rounded-full hover:scale-110 transition-transform duration-300" />
+          <CgMail className="text-[#D44638] text-5xl  hover:bg-white hover:shadow-lg hover:rounded-full hover:scale-110 transition-transform duration-300" />
         </div>
       </nav>
     </>
